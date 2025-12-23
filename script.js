6d11778791a8d1ae8bd86a630751ff745e626902
Gustavo Sales <salesgustavo003@gmail.com>
2025-12-23 12:54:44 -0300

    Commit To-Do (frontend)

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    const task = inputBox.value.trim();
+
+    if (task === "") {
+        alert("Sua tarefa está vazia!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = task;
+    listContainer.appendChild(li);
+
+    const span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li.appendChild(span);
+
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    const data = localStorage.getItem("data");
+    if (data) {
+        listContainer.innerHTML = data;
+    }
+}
+
+showTask();
